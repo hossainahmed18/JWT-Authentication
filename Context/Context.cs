e4078facf2120c1ea9e395db76be9eaaaad7e449
junayed <junayed@intellier.com>
2020-09-22 07:42:59 +0600

    authentication done

---
+++ Context/Context.cs
@@ -0,0 +1,19 @@
+using blogApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace blogApi.Context
+{
+   public class MainContext: DbContext{
+        public MainContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User>users{set;get;}
+
+    }
+}
---
+++ Migrations/20200921145541_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace blogApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    userId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    userName = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    userRole = table.Column<string>(nullable: false),
+                    password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.userId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
